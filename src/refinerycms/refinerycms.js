33cc21f784bda62c3ef43caa0c76d1a7a4439476
keram <keraml@gmail.com>
2011-04-10 21:35:05 +0200

    initial commit

---
+++ src/refinerycms/refinerycms.js
@@ -0,0 +1,19 @@
+/**
+ * The REFINERYCMS object is the single global object used by RefineryCMS.
+ * @version    $Id$
+ * @package    refinery-js
+ * @copyright  Copyright (C) 2011
+ * @author     keraM marek@keram.name http//keram.name
+ * @license    MIT
+ * @module     refinerycms
+ * @title      REFINERYCMS Global
+ * @required   jquery-1.5.2
+ */
+
+/**
+ * The REFINERYCMS global namespace object.
+ * @class REFINERYCMS
+ * @static
+ */
+var REFINERYCMS = REFINERYCMS || {};
+
---
+++ temp/yuidoc/refinerycms.js
@@ -0,0 +1,12 @@
+/**
+ * The REFINERYCMS object is the single global object used by RefineryCMS.
+ * @version    $Id$
+ * @package    refinery-js
+ * @copyright  Copyright (C) 2011
+ * @author     keraM marek@keram.name http//keram.name
+ * @license    MIT
+ * @module refinerycms
+ * @title  REFINERYCMS Global
+ */
+
+var REFINERYCMS = REFINERYCMS || {};
