a81e066732c9dedd41c8c9ef02c9d4ea0fdcf52b
Marek <keraml@gmail.com>
2011-04-19 05:54:36 +0200

    added menu from admin.js

---
+++ src/menu/js/menu.js
@@ -0,0 +1,75 @@
+/**
+ * Menu handling
+ *
+ * @author     keraM marek@keram.name http//keram.name
+ * @copyright  Copyright (C) 2011
+ * @license    MIT
+ * @version   0.1
+ */
+REFINERYCMS.menu = {
+	menu: null,
+
+	reorder: function (e, enable) {
+		e.preventDefault();
+		$('#menu_reorder, #menu_reorder_done').toggle();
+		$('#site_bar, #content').fadeTo(500, enable ? 0.35 : 1);
+
+		if(enable) {
+			this.menu.find('.tab a').click(function(ev){
+				ev.preventDefault();
+			});
+		} else {
+			this.menu.find('.tab a').unbind('click');
+		}
+
+		this.menu.sortable(enable ? 'enable' : 'disable');
+	},
+
+	init: function () {
+		var that = this;
+		this.menu = $('#menu');
+		if(this.menu.length === 0){
+			return;
+		}
+
+		this.menu.sortable({
+			axis: 'x',
+			cursor: 'crosshair',
+			connectWith: '.nested',
+			update: function(){
+				$.post('/refinery/update_menu_positions', this.menu.sortable('serialize', {
+					key: 'menu[]',
+					expression: /plugin_([\w]*)$/
+				}));
+			}
+		}).tabs();
+
+		this.menu.jcarousel({
+			vertical: false,
+			scroll: 1,
+			buttonNextHTML: "<img src='/images/refinery/carousel-right.png' alt='down' height='15' width='10' />",
+			buttonPrevHTML: "<img src='/images/refinery/carousel-left.png' alt='up' height='15' width='10' />",
+			listTag: this.menu.get(0).tagName.toLowerCase(),
+			itemTag: this.menu.children(':first').get(0).tagName.toLowerCase()
+		});
+
+		if (this.menu.outerWidth() < $('#page_container').outerWidth()) {
+			$("#page_container:not('.login #page_container')").corner('5px tr');
+		} else {
+			$("#page_container:not('.login #page_container')").uncorner();
+		}
+
+		//Initial status disabled
+		this.menu.sortable('disable');
+
+		this.menu.find('#menu_reorder').click(function(e){
+			that.reorder(e, true);
+		});
+
+		this.menu.find('#menu_reorder_done').click(function(e){
+			that.reorder(e, false);
+		});
+
+		this.menu.find('a').corner('top 5px');
+	}
+};
