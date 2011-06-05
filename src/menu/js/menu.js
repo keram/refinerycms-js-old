5efb9df7f7d102e51908404f2688025baf9815e7
Marek <keraml@gmail.com>
2011-06-05 18:47:46 +0200

    fix menu js parse error

--- src/menu/js/menu.js
+++ src/menu/js/menu.js
@@ -6,6 +6,9 @@
  * @license    MIT
  * @version   0.1
  */
+
+/*global REFINERYCMS, $, console, I18n */
+
 REFINERYCMS.menu = {
 	menu: null,
 
@@ -48,7 +51,7 @@ REFINERYCMS.menu = {
 			vertical: false,
 			scroll: 1,
 			buttonNextHTML: '<img src="/images/refinery/carousel-right.png" alt="down" height="15" width="10" />',
-			buttonPrevHTML: "<img src="/images/refinery/carousel-left.png" alt="up" height="15" width="10" />',
+			buttonPrevHTML: '<img src="/images/refinery/carousel-left.png" alt="up" height="15" width="10" />',
 			listTag: this.menu.get(0).tagName.toLowerCase(),
 			itemTag: this.menu.children(':first').get(0).tagName.toLowerCase()
 		});
